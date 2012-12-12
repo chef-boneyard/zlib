45342f78d2866c3b2f443e693df562317e37963d
jtimberman <joshua@opscode.com>
2012-12-12 11:38:42 -0700

    release v2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs zlib"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "2.0.0"
 
 recipe "zlib", "Installs zlib development package"
 
