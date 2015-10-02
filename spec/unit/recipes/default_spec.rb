6486128254ef21f165590ad57530ac10d66bc08a
Tim Smith <tsmith84@gmail.com>
2015-10-02 11:15:51 -0700

    Add chefspec test for Ubuntu

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'default recipe on Ubuntu 14.04' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new do |node|
+      node.automatic[:lsb][:codename] = 'trusty'
+    end.converge('zlib::default')
+  end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+
+  it 'should install zzsh package' do
+    expect(chef_run).to install_package('zlib1g-dev')
+  end
+end
